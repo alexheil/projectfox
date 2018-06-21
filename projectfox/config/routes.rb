60da0d4d0a7a8ffeb7c9847942e1193f1e886efa
alexheil <aheil4250@gmail.com>
2018-06-21 15:58:17 -0400

    user creation

--- projectfox/config/routes.rb
+++ projectfox/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+
+  devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations",  unlocks: "users/unlocks"}
+
+  devise_scope :user do
+    get 'sign_out', to: 'users/sessions#destroy'
+  end
+
 end
